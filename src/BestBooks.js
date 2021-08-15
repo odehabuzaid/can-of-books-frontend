be9181e4f5a2fd05d485bf305eaaf723768548c6
Odeh <abuzaid163@hotmail.com>
2021-08-15 22:07:50 +0300

    Initial

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
+
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
 
+
 class MyFavoriteBooks extends React.Component {
   render() {
-    return(
+    return (
       <Jumbotron>
         <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
+        <p>This is a collection of my favorite books</p>
       </Jumbotron>
-    )
+    );
   }
 }
-
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
---
+++ src/config.js
@@ -0,0 +1,15 @@
+import configJson from "./auth_config.json";
+
+export function getConfig() {
+  const audience =
+    process.env.REACT_APP_AUTH0_AUDIANCE && process.env.REACT_APP_AUTH0_AUDIANCE !== "Madrid"
+      ? process.env.REACT_APP_AUTH0_AUDIANCE
+      : null;
+
+  return {
+    domain:  process.env.REACT_APP_AUTH0_DOMAIN,
+    clientId:  process.env.REACT_APP_AUTH0_CLIENTID,
+    ...(audience ? { audience } : null),
+  };
+}
+
